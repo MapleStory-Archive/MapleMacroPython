6d85cee85a8b047f2b34316fbf9b18eb178eb5a3
k.kawabata srsys.jp <k.kawabata@srsys.jp>
2019-03-28 18:02:25 +0900

    UWSCParser.py update

--- Public/UWSCParser.py
+++ Public/UWSCParser.py
@@ -13,8 +13,20 @@
 import pyautogui
 import cv2
 import win32gui
+import pyperclip
+import math
+from datetime import datetime
 
-class Uwsc:
+global G_TIME_YY
+global G_TIME_MM
+global G_TIME_DD
+global G_TIME_HH
+global G_TIME_NN
+global G_TIME_SS
+global G_TIME_ZZ
+global G_TIME_WW
+
+class UwscFunction:
 
     """
     Comment : UWSC準拠のKBD関数
@@ -25,14 +37,18 @@ class Uwsc:
     Return  : None
     備考    :
     """
-    def KBD(self, key, type, time):
-        _time = time / 1000 # ミリ秒から秒へ変換
+    def KBD(self, key, type, ms):
+        sec = ms / 1000 # ミリ秒から秒へ変換
         if type == 0:
-            pyautogui.press(  key, interval = _time)
+            pyautogui.press(  key, interval = sec)
         elif type == 1:
-            pyautogui.keyDown(key, interval = _time)
+            pyautogui.keyDown(key, interval = sec)
         elif type == 2:
-            pyautogui.keyUp(  key, interval = _time)
+            pyautogui.keyUp(  key, interval = sec)
+
+
+
+
 
     """
     Comment : UWSC準拠のBTN関数
@@ -45,9 +61,14 @@ class Uwsc:
     Return  : None
     備考    :
     """
-    def BTN(self, key, type, xs, ys, time):
-        _time = time / 1000 # ミリ秒から秒へ変換
-        pyautogui.click(x=xs, y=ys, button=key, interval=_time)
+    def BTN(self, key, type, xs, ys, ms):
+        sec = ms / 1000 # ミリ秒から秒へ変換
+        pyautogui.click(x=xs, y=ys, button=key, interval=sec)
+
+
+
+
+
 
     """
     Comment : UWSC準拠の1回クリック専用BTN関数
@@ -59,9 +80,14 @@ class Uwsc:
     Return  : None
     備考    :
     """
-    def BTN(self, key, xs, ys, time):
-        _time = time / 1000 # ミリ秒から秒へ変換
-        pyautogui.click(x=xs, y=ys, button=key, interval=_time)
+    def BTN(self, key, xs, ys, ms):
+        sec = ms / 1000 # ミリ秒から秒へ変換
+        pyautogui.click(x=xs, y=ys, button=key, interval=sec)
+
+
+
+
+
 
     """
     Comment : UWSC準拠の画像認識関数(完全一致)
@@ -83,6 +109,11 @@ class Uwsc:
 
         return True
 
+
+
+
+
+
     """
     Comment : UWSC準拠の画像認識関数(テンプレートマッチング)
     Param   : $1 : self
@@ -97,12 +128,17 @@ class Uwsc:
               完全一致する画像がなかった : False
     備考    :
     """
-    def chkimg(self, picPath, transFlag, x1, y1, x2, y2, threshold):
+    def chkimgx(self, picPath, transFlag, x1, y1, x2, y2, threshold):
         im = pyautogui.locateOnScreen(picPath, region=(x1 , y1, x2, y2), confidence=threshold)
         if im is None:
             return False
 
         return True
+
+
+
+
+
 
     """
     Comment : ウィンドウ位置の変更
@@ -123,4 +159,75 @@ class Uwsc:
             return 1;
         cv2.moveWindow(windowName, xs, ys)
         return 0;
+
+
+
+    """
+    Comment : クリップボードへ文字列をコピー
+    Param   : $1 : self
+              $2 : コピーしたい文字列
+    Return  : None
+    備考    :
+    """
+    def SENDSTR(self, copyStr):
+        pyperclip.copy(copystr)
+
+
+
+    """
+    Comment : コマンドプロンプトからコマンドを実行する
+    Param   : $1 : self
+              $2 : 実行したいコマンド文字列
+    Return  : None
+    備考    : コマンドの終了は待たない
+    """   
+    def DOSCMD(self, cmd):
+        ret = subprocess.Popen(cmd, shell=True)
+
+
+
+
+    """
+    Comment : 現在時刻を取得します
+              また取得した時刻はグローバル変数に代入されます
+    Param   : $1 : self
+    Return  : 現在時刻
+    備考    : format : 1970-01-01 00:00:00
+    """
+    def GETTIME(self):
+        now = datetime.now()
+        G_TIME_YY = now.year
+        G_TIME_MM = now.month
+        G_TIME_DD = now.day
+        G_TIME_HH = now.hour
+        G_TIME_NN = now.minute
+        G_TIME_SS = now.second
+        G_TIME_ZZ = math.floor(now.microsecond/1000)
+        return now
+
+
+
+    """
+    Comment : 引数で
+    Param   : $1 : self
+              $2 : 取得したいx秒後の時間
+    Return  : 現在時刻 + $2 の時間
+    備考    : format : 1970-01-01 00:00:00
+    """
+    def GETTIME(self, afterSecondTime):
+        now = datetime.now()
+        afterTime = now + datetime.timedelta(seconds=afterSecondTime)
+        G_TIME_YY = now.year
+        G_TIME_MM = now.month
+        G_TIME_DD = now.day
+        G_TIME_HH = now.hour
+        G_TIME_NN = now.minute
+        G_TIME_SS = now.second
+        G_TIME_ZZ = math.floor(now.microsecond/1000)
+        return afterTime
+
+
+
+
+
 
